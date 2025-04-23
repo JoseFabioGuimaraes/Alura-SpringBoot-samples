0596db8ab3ad24e0feb0fe39265a251cffcfd9e9
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-04-23 11:58:18 -0300

    refactor datasource configuration to use environment variables

--- SpringBoot - JPA/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/ApiScreenMatchApplication.java
+++ SpringBoot - JPA/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/ApiScreenMatchApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiScreenMatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(ApiScreenMatchApplication.class, args);
 	}
 
