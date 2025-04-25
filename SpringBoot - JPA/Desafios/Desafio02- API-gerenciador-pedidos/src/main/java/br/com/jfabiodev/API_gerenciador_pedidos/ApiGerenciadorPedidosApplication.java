e7e7d42ad5acac7c7c99092852f663a017aa6b72
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-04-25 14:32:49 -0300

    first commit

---
+++ SpringBoot - JPA/Desafios/Desafio02- API-gerenciador-pedidos/src/main/java/br/com/jfabiodev/API_gerenciador_pedidos/ApiGerenciadorPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.jfabiodev.API_gerenciador_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorPedidosApplication.class, args);
+	}
+
+}
---
+++ SpringBoot - JPA/Desafios/Desafio02- API-gerenciador-pedidos/src/test/java/br/com/jfabiodev/API_gerenciador_pedidos/ApiGerenciadorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jfabiodev.API_gerenciador_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
