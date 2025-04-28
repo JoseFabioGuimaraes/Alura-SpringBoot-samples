32a0df3df725ffcd0093846ad24a112b6669cf17
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-04-28 10:53:18 -0300

    feat: add Episodio entity with JPA annotations and establish relationship with Serie

--- SpringBoot - JPA/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/model/Episodio.java
+++ SpringBoot - JPA/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/model/Episodio.java
@@ -1,14 +1,22 @@
 package br.com.jfabiodev.APIScreenMatch.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+@Entity
 public class Episodio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Integer temporada;
     private String titulo;
     private Integer numeroEpisodio;
     private Double avaliacao;
     private LocalDate dataLancamento;
+    @ManyToOne
+    private Serie serie;
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
@@ -26,6 +34,22 @@ public class Episodio {
         } catch (DateTimeParseException ex) {
             this.dataLancamento = null;
         }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
     }
 
     public Integer getTemporada() {
