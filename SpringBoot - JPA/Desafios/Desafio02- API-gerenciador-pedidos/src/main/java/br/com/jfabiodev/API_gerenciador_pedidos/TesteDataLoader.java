ee4324448f8e7970fd6e34ae0b277fc63289ad06
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-04-30 15:47:12 -0300

    feat: enhance TesteDataLoader to include Fornecedor and Pedido entities, improve product validation and relationships

--- SpringBoot - JPA/Desafios/Desafio02- API-gerenciador-pedidos/src/main/java/br/com/jfabiodev/API_gerenciador_pedidos/TesteDataLoader.java
+++ SpringBoot - JPA/Desafios/Desafio02- API-gerenciador-pedidos/src/main/java/br/com/jfabiodev/API_gerenciador_pedidos/TesteDataLoader.java
@@ -2,49 +2,91 @@ package br.com.jfabiodev.API_gerenciador_pedidos;
 
 import br.com.jfabiodev.API_gerenciador_pedidos.categoria.model.Categoria;
 import br.com.jfabiodev.API_gerenciador_pedidos.categoria.repository.CategoriaRepository;
+import br.com.jfabiodev.API_gerenciador_pedidos.fornecedor.model.Fornecedor;
+import br.com.jfabiodev.API_gerenciador_pedidos.fornecedor.repository.FornecedorRepository;
 import br.com.jfabiodev.API_gerenciador_pedidos.pedido.model.Pedido;
 import br.com.jfabiodev.API_gerenciador_pedidos.pedido.repository.PedidoRepository;
 import br.com.jfabiodev.API_gerenciador_pedidos.produto.model.Produto;
 import br.com.jfabiodev.API_gerenciador_pedidos.produto.repository.ProdutoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class TesteDataLoader {
+
     @Autowired
     private CategoriaRepository categoriaRepository;
 
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private FornecedorRepository fornecedorRepository;
+
+    @Transactional
     public void principal() {
         // Criando categorias
         Categoria categoriaEletronicos = new Categoria("Eletrônicos");
         Categoria categoriaLivros = new Categoria("Livros");
+        categoriaRepository.saveAll(Arrays.asList(categoriaEletronicos, categoriaLivros));
 
-        // Criando produtos e associando às categorias
+        // Criando fornecedores
+        Fornecedor fornecedorTech = new Fornecedor("Tech Supplier");
+        Fornecedor fornecedorLivros = new Fornecedor("Livraria Global");
+        fornecedorRepository.saveAll(Arrays.asList(fornecedorTech, fornecedorLivros));
+
+        // Criando produtos com validação
         Produto produto1 = new Produto("Notebook", 3500.0, categoriaEletronicos);
         Produto produto2 = new Produto("Smartphone", 2500.0, categoriaEletronicos);
         Produto produto3 = new Produto("Livro de Java", 100.0, categoriaLivros);
-        Produto produto4 = new Produto("Livro de Spring Boot", 150.0, categoriaLivros);
+        produto1.setFornecedor(fornecedorTech);
+        produto2.setFornecedor(fornecedorTech);
+        produto3.setFornecedor(fornecedorLivros);
 
-        // Associando produtos às categorias
-        categoriaEletronicos.setProdutos(List.of(produto1, produto2));
-        categoriaLivros.setProdutos(List.of(produto3, produto4));
+        List<Produto> produtos = Arrays.asList(produto1, produto2, produto3);
+        for (Produto produto : produtos) {
+            if (!produtoRepository.existsByNome(produto.getNome())) {
+                produtoRepository.save(produto);
+            } else {
+                System.out.println("Produto com o nome já existe: " + produto.getNome());
+            }
+        }
 
-        // Salvando categorias (cascateia produtos automaticamente, se configurado)
-        categoriaRepository.saveAll(List.of(categoriaEletronicos, categoriaLivros));
+        // Criando pedidos e associando produtos
+        Pedido pedido1 = new Pedido(LocalDate.now());
+        pedido1.setProdutoList(Arrays.asList(produto1, produto3));
+        Pedido pedido2 = new Pedido(LocalDate.now().minusDays(1));
+        pedido2.setProdutoList(Arrays.asList(produto2));
+        pedidoRepository.saveAll(Arrays.asList(pedido1, pedido2));
 
-        // Testando a persistência e o relacionamento
-        System.out.println("Categorias e seus produtos:");
-        categoriaRepository.findAll().forEach(categoria -> {
-            System.out.println("Categoria: " + categoria.getNome());
-            categoria.getProdutos().forEach(produto ->
-                    System.out.println(" - Produto: " + produto.getNome())
+        // Testando consultas e verificando os relacionamentos
+        System.out.println("Produtos na categoria Eletrônicos:");
+        categoriaRepository.findById(402L).ifPresent(categoria ->
+                categoria.getProdutos().forEach(produto ->
+                        System.out.println(" - " + produto.getNome())
+                )
+        );
+
+        System.out.println("\nPedidos e seus produtos:");
+        pedidoRepository.findAll().forEach(pedido -> {
+            System.out.println("Pedido " + pedido.getId() + ":");
+            pedido.getProdutos().forEach(produto ->
+                    System.out.println(" - " + produto.getNome())
             );
         });
+
+        System.out.println("\nProdutos e seus fornecedores:");
+        produtoRepository.findAll().forEach(produto ->
+                System.out.println("Produto: " + produto.getNome() +
+                        ", Fornecedor: " + produto.getFornecedor().getNome())
+        );
     }
 }
