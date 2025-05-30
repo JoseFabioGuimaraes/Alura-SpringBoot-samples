00c03a16c857a59d733910b737780d2cf91efa78
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-05-30 19:22:12 -0300

    feat: add CORS configuration and update SerieController endpoint

--- SpringBoot - SpringWeb/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/controller/SerieController.java
+++ SpringBoot - SpringWeb/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/controller/SerieController.java
@@ -1,7 +1,6 @@
 package br.com.jfabiodev.APIScreenMatch.controller;
 
 import br.com.jfabiodev.APIScreenMatch.dto.SerieDTO;
-import br.com.jfabiodev.APIScreenMatch.model.Serie;
 import br.com.jfabiodev.APIScreenMatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,7 @@ public class SerieController {
     @Autowired
     private SerieRepository repository;
 
-    @GetMapping("/serie")
+    @GetMapping("/series")
     public List<SerieDTO> obterSeries(){
         return repository.findAll().stream().map(s -> new SerieDTO(s.getId(),s.getTitulo(),s.getTotalTemporadas(),s.getAvaliacao(),
                 s.getGenero(),s.getAtores(),s.getPoster(),s.getSinopse())).collect(Collectors.toList());
