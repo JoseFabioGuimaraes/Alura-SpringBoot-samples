5ba53f3955035230fddb8633262d1fd57866c492
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-05-31 11:18:51 -0300

    feat: implement SerieService for series data handling and update SerieController endpoints

--- SpringBoot - SpringWeb/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/controller/SerieController.java
+++ SpringBoot - SpringWeb/APIScreenMatch/src/main/java/br/com/jfabiodev/APIScreenMatch/controller/SerieController.java
@@ -1,24 +1,40 @@
 package br.com.jfabiodev.APIScreenMatch.controller;
 
 import br.com.jfabiodev.APIScreenMatch.dto.SerieDTO;
-import br.com.jfabiodev.APIScreenMatch.repository.SerieRepository;
+import br.com.jfabiodev.APIScreenMatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
-    private SerieRepository repository;
+    private SerieService service;
 
-    @GetMapping("/series")
-    public List<SerieDTO> obterSeries(){
-        return repository.findAll().stream().map(s -> new SerieDTO(s.getId(),s.getTitulo(),s.getTotalTemporadas(),s.getAvaliacao(),
-                s.getGenero(),s.getAtores(),s.getPoster(),s.getSinopse())).collect(Collectors.toList());
+    @GetMapping
+    public List<SerieDTO> getAllSeries(){
+        return service.getAllSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> getTop5Series(){
+        return service.getTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> getNews(){
+        return service.getNews();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO getSerieById(@PathVariable Long id){
+        return service.getSerieById(id);
     }
 
 
