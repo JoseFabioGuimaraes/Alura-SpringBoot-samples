e992a1c90e79f26def776b7f15bc647d2915cbb3
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-06-09 19:10:33 -0300

    feat: implement JWT token generation and update authentication flow; add TokenService and TokenJWTDTO

--- SpringBoot - SpringWeb/MedVoll/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ SpringBoot - SpringWeb/MedVoll/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.Usuario;
 import med.voll.api.domain.usuario.UsuarioAutenticacaoDTO;
+import med.voll.api.infra.security.TokenJWTDTO;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,16 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
 
     private final AuthenticationManager manager;
+    private final TokenService tokenService;
 
-    public AutenticacaoController(AuthenticationManager manager){
+
+    public AutenticacaoController(AuthenticationManager manager, TokenService tokenService){
         this.manager = manager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid UsuarioAutenticacaoDTO json){
-        var token = new UsernamePasswordAuthenticationToken(json.login(), json.senha());
-        var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var AutenticationToken = new UsernamePasswordAuthenticationToken(json.login(), json.senha());
+        var authentication = manager.authenticate(AutenticationToken);
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new TokenJWTDTO(tokenJWT));
     }
 
 }
